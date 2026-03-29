f3ba2844787aac61bcf65e6bfa82d6bab0db43cb
Arnold Tyvern Madamombe <arnoldmadaz@gmail.com>
2026-03-29 10:18:47 +0200

    Fix JPA bootstrap configuration for EntityManager startup

--- backend/src/main/java/com/schoolms/SchoolMsApplication.java
+++ backend/src/main/java/com/schoolms/SchoolMsApplication.java
@@ -2,8 +2,12 @@ package com.schoolms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.schoolms")
+@EnableJpaRepositories(basePackages = "com.schoolms")
 public class SchoolMsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SchoolMsApplication.class, args);
